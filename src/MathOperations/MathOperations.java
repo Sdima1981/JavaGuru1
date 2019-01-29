53ac28a8c0b32e6dd9b4979caa8ca2e7620fe7a4
Sdima1981 <surigins@gmail.com>
2019-01-30 01:19:05 +0200

    User import

--- src/MathOperations/MathOperations.java
+++ src/MathOperations/MathOperations.java
@@ -6,18 +6,30 @@
 
 package MathOperations;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.Import;
+
+import java.util.Scanner;
+
 public class MathOperations {
     public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Введите первое число ");
+        int firstNumber = reader.nextInt();
+
+        System.out.println("Введите второе число ");
+        int secondNumber = reader.nextInt();
+        reader.close();
+        //System.out.println(n);
 
         String greeting = "Hello Everybody!!!";
         String answer = "How you doin...";
-        Integer firstNumber = 10;
-        Integer secondNumber = 20;
+        //Integer firstNumber = 10;
+        //Integer secondNumber = 20;
         int subtract = (secondNumber - firstNumber);
         int division = secondNumber / firstNumber;
         int sum = (firstNumber + secondNumber);
         int multiply = (firstNumber * secondNumber);
-        System.out.println("First number is 10;"); System.out.println("Second number is 20;");
+        System.out.println("First number is " + firstNumber + ";"); System.out.println("Second number is " + secondNumber + ";");
         System.out.println("This is the result of First number summed with Second number = " + sum + ";");
         System.out.println("This is the result of First number subtracted out of Second number = " + subtract + ";");
         System.out.println("This is the result of Second number divided by First number = " + division + ";");
